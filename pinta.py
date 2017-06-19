5be087c6b94866c99cf3c62f37c98df6d90eb0aa
Kisuke-CZE <kisuke@kisuke.cz>
2017-06-19 17:08:11 +0200

    Trochu prepsana Pinta.

--- pinta.py
+++ pinta.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import requests, sys, re
+import requests, sys, re, time, locale
 from bs4 import BeautifulSoup
+
+locale.setlocale(locale.LC_ALL,'')
 
 def get_url():
     return "http://www.pinta-restaurace.cz/denni_menu.php"
@@ -28,7 +30,9 @@ def return_menu(soup):
     #c = b.findNext("p").text
     #d = re.split(" Kč", c)
 
-    date = soup.find("td", { "style": "background: rgba(34, 15, 15, .30); font-size: 20px; border-bottom: 1px solid #b7a56d;" }).text
+    #date = soup.find_all("td", { "style": "background: rgba(34, 15, 15, .30); font-size: 20px; border-bottom: 1px solid #b7a56d;" })
+    #for day in date:
+    #    print(day)
     #date = ""
     #try:
         #date = ""
@@ -36,31 +40,35 @@ def return_menu(soup):
 
     #except:
     #    date = ""
-    jidlo_obsah =  soup.find_all("td", { "style":re.compile(r".*width: 100%.*") } )
+
+    #jidlo_obsah =  soup.find_all("td", { "style":re.compile(r".*width: 100%.*") } )
+    jidlo_obsah =  soup.find_all("tr" )
+
     jidlo_cena = []
-    arr = []
-    for i in range(0, len(jidlo_obsah)):
-        a = jidlo_obsah[i].text
-        if a == "Nápoj ZDARMA:":
-            break
-        try:
-            b = jidlo_obsah[i].findNext("td").text
-            if a is not "":
-                #print(a)
-                #print(b)
-                arr.append([a, b])
-        except Exception as ex:
-            print("Err" + str(ex))
+    items = []
+    today = time.strftime("%A %d.%m.%Y")
+    #print(today)
+    published = False
+    date = "???"
+    for i in jidlo_obsah:
+        if i.text != "":
+          #print (i.text)
+          if i.text.strip() == "Nápoj ZDARMA:" and published:
+              break
 
+          if published:
+              zradlo = i.find_all("td")
+              nazev = zradlo[1].text
+              cena = zradlo[2].text
+              #print(cena)
+              #print(nazev)
+              arr = [nazev, cena]
+              items.append(arr)
 
-        # jidlo_cena.append(jidlo_obsah[i].findNext("td").text)
-      #  arr.append([jidlo_obsah[i].text, jidlo_cena[i].text])
+          if i.text.strip() == today and not published:
+              published = True
+              date = today
 
-    items = arr
-
-
-    #print("ITEMS {0}".format(items))
-    #print("DATE", date)
     return(items, date)
 
 def result():
@@ -86,7 +94,5 @@ if __name__ == "__main__":
     bs = prepare_bs(file)
 
     #date = return_date(bs)
-    menu_list = return_menu(bs)
-    #print(menu_list)
-
-    #debug_print(date, menu_list)
+    menu_list, date = return_menu(bs)
+    #print(date, menu_list)
