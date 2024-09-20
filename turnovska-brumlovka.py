cf39713eb7095fe137defb5fd95d09a1cbfcb5a7
Kisuke-CZE <kisuke@kisuke.cz>
2024-09-20 17:24:52 +0200

    Updated turnovska pivnice

--- turnovska-brumlovka.py
+++ turnovska-brumlovka.py
@@ -30,56 +30,47 @@ def return_menu(soup):
     #dow = time.strftime("%A").upper()
     #dow = time.strftime("%A").lower()
     dow = time.strftime("%A")
-    fulldate = time.strftime("%-d. %-m. %Y").upper()
-    #print(dow + " " + fulldate)
+    fulldate = time.strftime("%-d. %-m. %Y")
+    # print(dow + " " + fulldate)
     #dow = "PONDĚLÍ"
     #fulldate = "21. 11. 2022"
     tablenum=1
-
-    a = soup.find("div", { "class": "daily-menu" }).div.find_next_sibling("div").div.div
+    
+    #a = soup.find("div", { "class": "daily-menu" }).div.find_next_sibling("div").div.div
+    a = soup.find("div", { "class": "daily-menu" }).div.div.div
     #print(a)
 
-    dates = a.find_all("h4")
+    dates = a.find_all("h4", { "class": "weekday", "data-lang": "cz"})
+    lasttab = 1
     for index,item in enumerate(dates):
-        datematch = re.match(dow + "\s+" + fulldate, item.text, re.IGNORECASE)
+        datematch = re.match(dow + "\\s+" + fulldate, item.text.strip(), re.IGNORECASE)
+        lasttab = index
+        # print(item.text.strip())
         if datematch:
-            #print(item.text)
             tablenum = index
 
-    date = dates[tablenum].text
+    date = dates[tablenum].text.strip()
     tables = a.find_all("table", recursive=False)
-    #lasttab = 4 * tablenum
-    lasttab = 5 * tablenum
+    # lasttab = 4 * tablenum
+    # lasttab = 5 * tablenum
     items = []
-    matchzradlo = "([\w\d\sěščřžýáíéúůóÓĚŠČŘŽÝÁÍÉÚŮöäëÄÖËťŤ„“\"\(\)\,\-\+]+)[\s]+([0-9]{2,3}[\s]*(Kč|,-))"
-    for table in range(lasttab-4, lasttab):
-        zradla = tables[table].tbody.find_all("tr", recursive=False)
-        for item in zradla:
-            #print(item)
-            subtables = item.find_all("table")
-            if subtables:
-                for subtable in subtables:
-                    subzradla = subtable.find_all("tr")
-                    for subitem in subzradla:
-                        subtext = subitem.text
-                        arr = []
-                        submatch = re.match(matchzradlo, subtext)
-                        if submatch is not None:
-                            arr = [submatch.group(1).strip(), submatch.group(2).strip()]
-                            items.append(arr)
-                        else:
-                            continue
-
-            elif item.text:
-                text = item.text
-                arr = []
-                match = re.match(matchzradlo, text)
-                if match is not None:
-                    arr = [match.group(1).strip(), match.group(2).strip()]
-                else:
-                    continue
-                items.append(arr)
-
+    # matchzradlo = "([\\w\\d\\sěščřžýáíéúůóÓĚŠČŘŽÝÁÍÉÚŮöäëÄÖËťŤ„“\"\\(\\)\\,\\-\\+]+)[\\s]+([0-9]{2,3}[\\s]*(Kč|,-))"
+    zradla = tables[tablenum].find_all("tr", recursive=False)
+    for item in zradla:
+      #if item.text:
+      #  text = item.text
+      #  arr = []
+      #  match = re.match(matchzradlo, text)
+      #  if match is not None:
+      #    arr = [match.group(1).strip(), match.group(2).strip()]
+      #  else:
+      #    continue
+      if item.td.span is None:
+        continue
+      nazev = item.td.find("span", { "data-lang": "cz" }).text.strip()
+      cena = item.find_all("td")[1].text.strip()
+      arr = [nazev, cena]
+      items.append(arr)
     return(items, date)
 
 
