79cbd7431630f53ecb7ca62b6154ab1d80e1e4bb
Kisuke-CZE <kisuke@kisuke.cz>
2025-05-21 16:06:46 +0200

    Uprava na nove PDF menu u Michelske

--- michelska.py
+++ michelska.py
@@ -1,81 +1,83 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import requests, sys, re
+import requests, sys, re, tempfile, os
 from datetime import datetime
+from subprocess import check_output
 from bs4 import BeautifulSoup
 
 def get_url():
-    return "http://www.michelska.cz/dnes.htm"
+    return "http://www.michelska.cz/denni-menu"
 
 def get_name():
     return "Michelská"
 
 def get_file():
+    print("Stahuji menu")
     user_agent = {'User-agent': 'Mozilla/5.0'}
-    kantyna = requests.get(get_url(), headers = user_agent)
-    kantyna.encoding = 'Windows-1250'
-    return kantyna
+    menupage = requests.get(get_url(), headers = user_agent)
+    antiword = None
+    if menupage is not None and menupage.status_code == 200:
+      htmlcode = BeautifulSoup(menupage.text, 'html.parser')
+      gdrive_link = htmlcode.find("div", {"class": "pdf-iframe-container"} ).find("iframe")['src']
+      gdrive_id = re.match(".*file\\/d\\/(.*)\\/preview", gdrive_link)[1]
+      gdrive_down = "https://drive.usercontent.google.com/download?id=" + gdrive_id + "&export=download&authuser=0"
+      pdf_stream = requests.get(gdrive_down, stream=True, timeout=6)
+      # print(gdrive_down)
+      tmp_fd,tmp_path = tempfile.mkstemp()
+      with open(tmp_path, "wb") as f:
+        for chunk in pdf_stream.iter_content(chunk_size=1024):
+          f.write(chunk)
+      os.close(tmp_fd)
+      
+      print("menu stazeno, prevadim na text")
+      antiword = check_output(["pdftotext", "-layout", tmp_path, "-"]).decode("utf8")
+      os.remove(tmp_path)
+      print("prevedeno na text")
+    return antiword
+    
+def debug_print(date, menu):
+    print(date)
+    print(menu)
 
-def prepare_bs(kantyna):
-    if kantyna is not None and kantyna.status_code == 200:
-        html = kantyna.text
-        soup = BeautifulSoup(html, 'html.parser')
-        return soup
-
-    else:
-        return "Error"
-
-def return_menu(soup):
-    date = soup.find_all("p", {"class": "MsoNormal"})[1].text.strip().strip('Nabídka na')
-    a = soup.find("table").find_all("tr")
-    previous = ''
+def return_menu(antiword):
+    #print(antiword)
     items = []
-    for item in a:
-        # print(item)
-        # print(previous)
-        grid = item.find_all("td")
-        jidlo = grid[1].text.strip()
-        # print(grid[2].text)
-        predcena = re.match("([0-9]{2,3}[\s]*,-).*", grid[2].text.strip())
-        if predcena:
-            cena = predcena.group(1).strip()
-            if jidlo and cena:
-                if previous == 'Salát':
-                    jidlo = jidlo + ' salát'
-                arr = [jidlo, cena]
-                items.append(arr)
-            else:
+    prev_match = False
+    date = "???"
+    
+    for item in antiword.splitlines():
+        match = re.match('\\s*([0-9]+[a-z]+)?([A-Za-z0-9ěščřžýáíéůúťňóöďŤĚŠČŘŽŇÝÁÍÉÚŮÓÖĎ \t,\\-–“„\\(\\)´\\/]+)[\\s\n]+([0-9]+)\\s?,-\\s*', item)
+        match_date = re.match('Nabídka na (.*)', item.strip())
+        if match:
+            nazev = match.group(2).strip()
+            cena = match.group(3).strip()
+            if nazev and cena:
+                items.append([nazev, cena + ' Kč'])
                 continue
-        else:
-            previous = item.text.replace(':', '').strip()
-            continue
-    return(items, date)
+        elif match_date:
+            date = match_date.group(1)
+    return(date, items)
 
 
 def result():
+    lokalita = "brumlovka"
     try:
-        file = get_file()
-
-        bs = prepare_bs(file)
-
+        page = get_file()
+        date, menu_list = return_menu(page)
         nazev = get_name()
         url = get_url()
-        lokalita = "brumlovka"
 
-        menu_list, date = return_menu(bs)
 
         return (nazev, url, date, menu_list, lokalita)
-    except Exception as e:
-        print(e)
+    except:
+        #return(get_name() + " - Chyba", "", "Chyba", ["", "", ""])
         nazev = get_name()
         url = get_url()
-        lokalita = "brumlovka"
         return (nazev, url, "Menu nenalezeno", [], lokalita)
 
+    os.remove(TMP)
+
 if __name__ == "__main__":
-    file = get_file()
-
-    bs = prepare_bs(file)
-
-    menu_list, date = return_menu(bs)
-    print (date, menu_list)
+    page = get_file()
+    date, menu_list = return_menu(page)
+    debug_print(date, menu_list)
